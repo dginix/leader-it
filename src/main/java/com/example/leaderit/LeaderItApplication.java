50ac0984efa2eaae8b14f9a1c3462019b51012b1
dginix <kruchininds@yandex.ru>
2023-03-09 19:22:54 +0500

    Initial commit

---
+++ src/main/java/com/example/leaderit/LeaderItApplication.java
@@ -0,0 +1,13 @@
+package com.example.leaderit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/leaderit/LeaderItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaderit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
