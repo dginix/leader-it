5bf720743e4d6e4bd0df636c67458e68ba61bad0
dginix <kruchininds@yandex.ru>
2023-03-10 12:40:11 +0500

    feat: добалвен repository layer для IotDevice

---
+++ src/main/java/com/example/leaderit/repository/IotDeviceRepository.java
@@ -0,0 +1,18 @@
+package com.example.leaderit.repository;
+
+import com.example.leaderit.entity.IotDevice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Repository
+public interface IotDeviceRepository extends JpaRepository<IotDevice, Long> {
+    IotDevice findBySerialNumber(String serialNumber);
+    Page<IotDevice> findByDeviceTypeIn(List<String> deviceType, Pageable pageable);
+    Page<IotDevice> findByDateAddedBetween(LocalDateTime fromDate, LocalDateTime toDate, Pageable pageable);
+}
