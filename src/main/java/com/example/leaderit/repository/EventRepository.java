0a12b6134d1f34526c0b432e0cb524234ded0298
dginix <kruchininds@yandex.ru>
2023-03-10 12:41:06 +0500

    feat: добалвен repository layer для Event

---
+++ src/main/java/com/example/leaderit/repository/EventRepository.java
@@ -0,0 +1,14 @@
+package com.example.leaderit.repository;
+
+import com.example.leaderit.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+    Page<Event> findByIotDeviceSerialNumberAndDateCreatedBetween(String serialNumber, LocalDateTime fromDate, LocalDateTime toDate, Pageable pageable);
+}
