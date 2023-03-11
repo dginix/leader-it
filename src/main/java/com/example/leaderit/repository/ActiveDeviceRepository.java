6a99c76823191ffbf408fbe113fd25495144f66d
dginix <kruchininds@yandex.ru>
2023-03-11 22:29:54 +0500

    fix: метод с удалением записей об активных устройствах теперь @Transactional

--- src/main/java/com/example/leaderit/repository/ActiveDeviceRepository.java
+++ src/main/java/com/example/leaderit/repository/ActiveDeviceRepository.java
@@ -1,23 +1,22 @@
 package com.example.leaderit.repository;
 
 import com.example.leaderit.entity.ActiveDevice;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface ActiveDeviceRepository extends JpaRepository<ActiveDevice, Long> {
-    Page<ActiveDevice> findAll(Pageable page);
     Optional<ActiveDevice> findByIotDeviceId(Long Long);
 
-    // TODO отнимать 30 минут в сервисе
     @Modifying
+    @Transactional
     @Query("DELETE FROM ActiveDevice a WHERE a.dateLastActivity < :inactiveTime")
     void deleteInactiveDevices(@Param("inactiveTime") LocalDateTime inactiveTime);
 }
--- src/main/java/com/example/leaderit/service/EventServiceImpl.java
+++ src/main/java/com/example/leaderit/service/EventServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
@@ -38,8 +39,7 @@ public class EventServiceImpl implements EventService {
         this.activeDeviceService = activeDeviceService;
     }
 
-    @Autowired
-
+    @Transactional
     @Override
     public EventDto addEventBySerialNumber(String serialNumber, String secretKey, @Valid EventDto eventDto) {
         IotDevice iotDevice = iotDeviceRepository.findBySerialNumber(serialNumber).orElseThrow(
